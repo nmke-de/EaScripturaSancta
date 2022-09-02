61a8487b2edba72e772ab8bb47e456ba6d1672c1
nmke-de <github@nmke.de>
2022-09-02 18:20:33 +0200

    Fixed bib/index.php

--- bib/index.php
+++ bib/index.php
@@ -7,7 +7,7 @@
 $ls = shell_exec("ls | grep -v index.php");
 $bibles = explode("\n",$ls);
 foreach ($bibles as $bible){
-	echo "<li><a href='https://www.nmke.de/$bible.bib'>$bible</a></li>\n";	
+	echo "<li><a href='$bible'>$bible</a></li>\n";	
 }
 
 ?>
