12000a5c41eb4bbfdf61098c30fa57f8e7c0f97e
nmke-de <github@nmke.de>
2022-12-02 22:30:14 +0100

    Fixed style

--- index.php
+++ index.php
@@ -76,7 +76,7 @@ if (!$embed) {
 <html>
 <head>
 <meta charset='utf-8' />
-<link rel='stylesheet' type='text/css' href='lunil.css' />
+<link rel='stylesheet' type='text/css' href='./lunil.css' />
 <link rel='shortcut icon' type='image/svg' href='icon.svg' />
 <title>ESS - elb</title>
 </head>
